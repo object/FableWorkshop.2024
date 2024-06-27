74ad2aabc6b4ac53f084fd7b02f00beedbc2b50c
Vagif Abilov <vagif.abilov@nrk.no>
2024-06-27 21:49:10 +0200

    Updated client with socket message support

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -2,10 +2,15 @@ module App
 
 open Elmish
 open Elmish.React
+open Elmish.Bridge
 
 open Update
 open View
 
 Program.mkProgram init update view
+|> Program.withBridgeConfig (
+    Bridge.endpoint "ws://localhost:5000/socket"     
+    |> Bridge.withUrlMode Raw
+    |> Bridge.withMapping (fun (x : Shared.Sockets.ClientMessage) -> x |> Messages.MediaSetEvent))
 |> Program.withReactSynchronous "elmish-app"
 |> Program.run
